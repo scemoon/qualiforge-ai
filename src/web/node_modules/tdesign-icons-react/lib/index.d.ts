71fb264d03f9dc21c84508db580532b1e728c7b7
scemoon <mengsan8325150@gmail.com>
2026-05-07 06:15:56 +0000

    clean dirty

--- src/web/node_modules/tdesign-icons-react/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './icons';
-export * from './manifest';
-export * from './icon';
-export * from './iconfont/iconfont';
-export * from './svg-sprite/svg-sprite';
-export * from './global-config';
