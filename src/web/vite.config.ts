675c570b7743722a7a84d5e6ca62d1875766b878
scemoon <san@scemoons-Mac-mini.local>
2026-05-11 17:17:40 +0800

    update: sr

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -3,18 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/forge/',
+  base: './',
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        // 内容哈希作为版本号，确保 CDN 缓存失效
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: "assets/[name]-v2-[hash].js",
-      },
-    },
+    // emptyOutDir: true,
+    // rollupOptions: {
+    //   output: {
+    //     // 内容哈希作为版本号，确保 CDN 缓存失效
+    //     assetFileNames: 'assets/[name]-[hash][extname]',
+    //     chunkFileNames: 'assets/[name]-[hash].js',
+    //     entryFileNames: "assets/[name]-v2-[hash].js",
+    //   },
+    // },
   },
   resolve: {
     alias: {
